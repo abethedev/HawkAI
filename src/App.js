d3963ee39657885fb8faf224d973afd2390d611f
Abe <timtamultra@gmail.com>
2022-01-24 11:06:32 +1100

    removed comment

--- src/App.js
+++ src/App.js
@@ -86,9 +86,6 @@ class App extends Component {
             zoom={14}
             scrollWheelZoom={true}
           >
-            {/* <button id="trigger">
-              <LocationMarker></LocationMarker>click
-            </button> */}
             <LocationMarker></LocationMarker>
 
             <LayersControl
